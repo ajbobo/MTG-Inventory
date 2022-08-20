9681f67cb9b055ce912eddea0b4baad0cd83f0a5
A.J. Bobo <aj.bobo@niceincontact.com>
2022-08-20 13:27:09 -0600

    Moved RefreshableDialog to its own file Renamed file with EditCardDialog

--- term/EditCardDialog.cs
+++ term/EditCardDialog.cs
@@ -1,40 +1,9 @@
 using System.Text;
-using Microsoft.Data.Sqlite;
 using Terminal.Gui;
 using static MTG_CLI.SQLManager.InternalQuery;
 
 namespace MTG_CLI
 {
-    abstract class RefreshableDialog
-    {
-        static int cnt = 0;
-
-        protected void ClearTmpViews(Dialog dlg)
-        {
-            if (dlg.Visible)
-            {
-                List<View> removable = new();
-                foreach (View view in dlg.Subviews[0].Subviews)
-                {
-                    if (view.Id.StartsWith("tmp"))
-                    {
-                        removable.Add(view);
-                    }
-                }
-                foreach (View view in removable)
-                    dlg.Subviews[0].Remove(view);
-                dlg.SetNeedsDisplay();
-            }
-        }
-
-        protected T tmpView<T>(T view) where T : View
-        {
-            view.Id = "tmp" + cnt;
-            cnt++;
-            return view;
-        }
-    }
-
     class EditCardDialog : RefreshableDialog
     {
         public event Action? DataChanged;
