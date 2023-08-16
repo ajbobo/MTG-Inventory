039d9da454bc463f235d3e3e0f6a38ea71b54d4a
ajbobo <ajb.codemonkey@gmail.com>
2023-08-16 13:28:54 -0600

    Updated API ideas Reorganized some files Removed some sample files that I don't need any more

--- scripts/GetFilteredSets.ps1
+++ scripts/GetFilteredSets.ps1
@@ -8,7 +8,7 @@ $filtered = $sets |
     Where-Object set_type -In ('core', 'expansion', 'masterpiece', 'masters', 'commander', 'draft_innovation', 'funny')
 Write-Host "Filtered Sets: $($filtered.Length)"
 
-$collectable = $filtered | Where {
+$collectable = $filtered | Where-Object {
     ($_.set_type -ne 'funny' -and $_.parent_set_code -ne 'sld') -or ($_.block_code -eq $null -and $_.parent_set_code -eq $null) 
 }
 Write-Host "Collectable sets: $($collectable.Length)"
