eba84fc3e5d4393374fba422dd7a3688830cb793
A.J. Bobo <aj.bobo@niceincontact.com>
2023-10-07 09:28:53 -0600

    Updates to Github Actions script

--- api/Program.cs
+++ api/Program.cs
@@ -31,11 +31,11 @@ public class Program
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
+        // if (app.Environment.IsDevelopment())
+        // {
             app.UseSwagger();
             app.UseSwaggerUI();
-        }
+        // }
 
         app.UseHttpsRedirection();
         app.UseAuthorization();
