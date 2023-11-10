509bfeddedbb8fc11efb2533b2877db56fb2870c
A.J. Bobo <aj.bobo@niceincontact.com>
2023-11-09 21:31:33 -0700

    Set and Card names are shown in the TItle bar Set default size for Windows

--- mauiapp/App.xaml.cs
+++ mauiapp/App.xaml.cs
@@ -7,5 +7,17 @@ public partial class App : Application
 		InitializeComponent();
 
 		MainPage = new AppShell();
-    }
+	}
+
+#if WINDOWS
+	protected override Window CreateWindow(IActivationState activationState)
+	{
+		Window window = base.CreateWindow(activationState);
+
+		window.Height = 800;
+		window.Width = 600;
+
+		return window;
+	}
+#endif
 }
