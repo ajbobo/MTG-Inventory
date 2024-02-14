d063fdb09cb7ebe626c486c525f15ee2dfcd7f75
A.J. Bobo <aj.bobo@niceincontact.com>
2024-02-14 11:06:00 -0700

    Configured Angular proxy to get around CORS

--- ng-web-ui/src/app/inventory.service.ts
+++ ng-web-ui/src/app/inventory.service.ts
@@ -8,31 +8,12 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class InventoryService {
-  private apiUrl: string = 'https://mtg-inventory.azurewebsites.net/api'
+  private apiUrl: string = '/api'; // The proxy.conf.json file connects this to the full URL
 
   constructor(private http: HttpClient) { }
 
   getSetList() : Observable<MTG_Set[]> {
     const url: string = `${this.apiUrl}/Sets`
     return this.http.get<MTG_Set[]>(url); // This should have error handling and maybe logging
-
-    // TODO: This needs to call the API
-    // return of([
-    //   {
-    //     code: "pip",
-    //     name: "Fallout",
-    //     iconUrl: "https://svgs.scryfall.io/sets/pip.svg?1707714000"
-    //   },
-    //   {
-    //     code: "clu",
-    //     name: "Ravnica: Clue Edition",
-    //     iconUrl: "https://svgs.scryfall.io/sets/clu.svg?1707714000"
-    //   },
-    //   {
-    //     code: "mkm",
-    //     name: "Murders at Karlov Manor",
-    //     iconUrl: "https://svgs.scryfall.io/sets/mkm.svg?1707714000"
-    //   }
-    // ])
   }
 }
