07a44c1f443c6d342ba9680772b858f6b15b801e
A.J. Bobo <aj.bobo@niceincontact.com>
2025-07-09 11:25:06 -0600

    Added start of a login page    Has all the controls, but no behavior

--- ng-web-ui/src/app/app.routes.ts
+++ ng-web-ui/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { CollectionPanelComponent } from './collection-panel/collection-panel.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: 'collection/:setCode/:cardNumber', component: CollectionPanelComponent },
     { path: 'collection/:setCode', component: CollectionPanelComponent },
     { path: 'collection', component: CollectionPanelComponent },
-    { path: '', redirectTo: 'collection', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: '**', component: PageNotFoundComponent}
 ];
