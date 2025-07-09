bd91d0c39e5e7028785f967d91d7089b4260e254
A.J. Bobo <aj.bobo@niceincontact.com>
2025-07-09 14:49:08 -0600

    Added UIs for Register and Reset Password    Still no actual behavior

--- ng-web-ui/src/app/app.routes.ts
+++ ng-web-ui/src/app/app.routes.ts
@@ -2,12 +2,16 @@ import { Routes } from '@angular/router';
 import { CollectionPanelComponent } from './collection-panel/collection-panel.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
 
 export const routes: Routes = [
     { path: 'collection/:setCode/:cardNumber', component: CollectionPanelComponent },
     { path: 'collection/:setCode', component: CollectionPanelComponent },
     { path: 'collection', component: CollectionPanelComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'resetpassword', component: ResetpasswordComponent },
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: '**', component: PageNotFoundComponent}
 ];
---
+++ ng-web-ui/src/app/resetpassword/resetpassword.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-resetpassword',
+  standalone: true,
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './resetpassword.component.html',
+  styleUrl: './resetpassword.component.scss'
+})
+export class ResetpasswordComponent {
+
+}
